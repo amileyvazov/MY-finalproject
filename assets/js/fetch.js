2e500cf12e832d2f23bbae36e00262a6b84c09c1
unknown <git@github.com:amileyvazov/jq.task.git>
2023-06-08 21:41:19 +0200

    home page finished structed

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -5,16 +5,26 @@ function getDataBase(){
         data.forEach(el => {
             const rowCard=document.querySelector('#card-sec .row')
            rowCard.innerHTML +=`
-           <div class="col-5  m-5">
-             <div class="card-img"><img src="${el.avatar}" alt=""></div>
+           <div class="col-sm-12 card col-xl-4 col-lg-3 text-center ">
+             <div class="card-img"><img class="w-100" src="${el.avatar}" alt=""></div>
              <div class="card-body">
+                
                <div class="card-text">
                 <h2>${el.name}<h2>
                 <p>${el.text}<p>
                 </div>
                <div class="card-foot d-flex justify-content-between">
                 <div class="card-price">
-                    <span>$${el.price}</span>
+                    <span class="d-flex "> 
+                    <i class="fa-solid fa-0 px-2"></i> 
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-solid fa-0 px-2"></i>
+                    </span>
+                    <a href="">$${el.price}</a>
                 </div>
                 <div class="cars-btn d-flex justify-content-end">
                 <a>Add to Card</a>
